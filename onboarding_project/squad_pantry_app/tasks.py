82865d7b848e123cfe7f67ee86b4359cdbd5997c
prakhar96 <prakhars@squadrun.co>
2018-02-07 19:18:49 +0530

    Added Tasks

--- onboarding_project/squad_pantry_app/tasks.py
+++ onboarding_project/squad_pantry_app/tasks.py
@@ -1,7 +1,25 @@
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app.config_from_object('celeryconfig')
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour=11, minute=59),
+        throughput.s(),
+    )
+
+    sender.add_periodic_task(
+        crontab(hour=11, minute=59),
+        turnaround_time.s(),
+    )
 
 @app.task
 def throughput():
     return 4
+
+@app.task
+def turnaround_time():
+    return 5
