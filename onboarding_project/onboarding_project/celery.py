ec6c0e99b5526c35db2b3e21a1d97b51ab47ef03
prakhar96 <prakhars@squadrun.co>
2018-02-09 18:13:53 +0530

    Minor Fixes

--- onboarding_project/onboarding_project/celery.py
+++ onboarding_project/onboarding_project/celery.py
@@ -13,7 +13,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 from squad_pantry_app.models import ConfigurationSettings
 schedule_interval = int(ConfigurationSettings.objects.get(constant='INTERVAL').value)
 
-
 app.conf.beat_schedule = {
     'add-every-10-seconds': {
         'task': 'onboarding_project.tasks.calc_performance_metrics',
